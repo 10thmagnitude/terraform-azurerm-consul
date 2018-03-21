ebacbf7c5eb3875141202a9510795899daccf502
Scott Nowicki <scott@Scotts-MacBook-Pro.local>
2018-03-21 18:52:49 -0500

    additional outputs, and cleanup

--- variables.tf
+++ variables.tf
@@ -89,11 +89,6 @@ variable "num_servers" {
   default     = 3
 }
 
-variable "num_clients" {
-  description = "The number of Consul client nodes to deploy. You typically run the Consul client alongside your apps, so set this value to however many Instances make sense for your app code."
-  default     = 1
-}
-
 variable "cluster_tag_key" {
   description = "The tag the Azure Instances will look for to automatically discover each other and form a cluster."
   default     = "consul-servers"
